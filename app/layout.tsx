6f5c197e058367f31c09ad0cc756326ac1661c95
AlexSearha <contact@mytechcompanion.fr>
2026-04-07 16:44:43 +0200

    smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,8 @@ export default function RootLayout({
   return (
     <html
       lang="fr"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
+      data-scroll-behavior="smooth"
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
